be7235cf27e74f9a4159c3f1d575582815f59e51
mark-wilsdorf <markwilsdorfwork@gmail.com>
2019-08-28 21:11:18 +0300

    Добавил config.yml, убрал ненужное из utils.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const sql = require("mssql");
 const utils = require("./utils");
+const config = require("config-yml");
 
-const pool = new sql.ConnectionPool(utils.config);
-
+const pool = new sql.ConnectionPool(config.connectionConfig);
 pool.connect().then(pool => {
     return pool.query(utils.query);
 }).then(result => {
--- utils.js
+++ utils.js
@@ -4,45 +4,6 @@ const queryString = `
     on sc.Id = cont.SysSchemaId
     where sc.ManagerName = 'SourceCodeSchemaManager'`;
 
-//TODO FIXME: вынести все настройки доступов в отдельный файл, использовать config-yml, добавить этот конфигурационный файл в gitignore
-const sqlConfig = {
-    user: "msp",
-    password: "msp",
-    server: "localhost",
-    database: "test44",
-    port: 1433,
-    pool: {
-        max: 500,
-        min: 0,
-        idleTimeoutMillis: 30000
-    }
-};
-
-const accessModifiers = {
-    private: "private",
-    public: "public",
-    protected: "protected",
-    internal: "internal",
-    protectedInternal: "protected internal",
-    privateProtected: "private protected"
-};
-
-const classType = {
-    default: "",
-    abstract: "abstract",
-    sealed: "sealed",
-    static: "static",
-    partial: "partial"
-};
-
-const methodType = {
-
-};
-
 module.exports = {
     query: queryString,
-    config: sqlConfig,
-    access: accessModifiers,
-    classType: classType,
-    methodType: methodType
 };
